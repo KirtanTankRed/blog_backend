947ab15ef99dfc539fe1eade40f9220938f0045b
KirtanTank <kirtan.tank@redsoftware.in>
2025-01-25 14:19:24 +0530

    Add Initial Express server setup
    Set up a basic Express.js server for the blog backend. This includes:
    - Creating a simple server instance
    - Setting up a default route to test server functionality

---
+++ server.js
@@ -0,0 +1,21 @@
+//server.js
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+//Middleware to handle json (built in with express)
+app.use(express.json()) // Returns middleware that only parses json
+
+//Port from envrionment variables or default to 5000
+const PORT = process.env.PORT || 5000;
+
+//Test route
+app.get('/', (request,response) => {
+    response.send("Server is running!")
+});
+
+//Start the server
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port: http://localhost: ${PORT}`)
+})
